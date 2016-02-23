28ce805d5f855683ee8542c3b4fb0681d202824c
Keita Nakamura <nakamura-keita-kn@ynu.jp>
2016-02-23 18:59:24 +0900

    add install.sh file

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -s ~/dotfiles/_agignore ~/.agignore
+ln -s ~/dotfiles/_gvimrc ~/.gvimrc
+ln -s ~/dotfiles/_latexmkrc ~/.latexmkrc
+ln -s ~/dotfiles/_vimrc ~/.vimrc
+ln -s ~/dotfiles/_zshrc ~/.zshrc
+ln -s ~/dotfiles/_vim/spell ~/.vim/spell
+ln -s ~/dotfiles/_vim/template ~/.vim/template
