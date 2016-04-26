59bc7ca1f3f5ec8bec9aede6e0141f7654e435c2
Keita Nakamura <nakamura-keita-kn@ynu.jp>
2016-04-26 18:41:11 +0900

    update

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -s ~/dotfiles/_gvimrc ~/.gvimrc
 ln -s ~/dotfiles/_latexmkrc ~/.latexmkrc
 ln -s ~/dotfiles/_vimrc ~/.vimrc
 ln -s ~/dotfiles/_zshrc ~/.zshrc
+ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/_vim/spell ~/.vim/spell
 ln -s ~/dotfiles/_vim/template ~/.vim/template
