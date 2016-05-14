8f2c231959ed9aa4968c588464a7d5c21090ad57
Keita Nakamura <nakamura-keita-kn@ynu.jp>
2016-05-14 19:25:50 +0900

    cleanup

--- sh/auto-complete.zsh
+++ sh/auto-complete.zsh
@@ -1,8 +1,9 @@
 autoload -U compinit
 zle -N zcom::complete-on
 zle -N zcom::complete-off
-# zle -N zcom::expand-or-complete
 compinit
+
+ZCOM_LIST_MAX=20
 
 function zcom::binded-function()
 {
@@ -16,7 +17,6 @@ function zcom::complete-on()
   ORIG_DELETE="`zcom::binded-function "backward-delete-char"`"
   zle -N self-insert zcom::self-insert
   zle -N backward-delete-char zcom::backward-delete-char
-  # zle -N expand-or-complete zcom::expand-or-complete
 }
 
 function zcom::complete-off()
@@ -37,7 +37,7 @@ function zcom::limit-list()
 {
   if ((compstate[nmatches] <= 1)); then
     zle -M ""
-  elif ((compstate[list_lines] > 40)); then
+  elif ((compstate[list_lines] > ZCOM_LIST_MAX)); then
     compstate[list]=""
     zle -M "too many matches."
   fi
@@ -79,39 +79,6 @@ function zcom::backward-delete-char
 {
   ((CURSOR--))
   BUFFER="$LBUFFER"
-
-  # if [[ "$LBUFFER" != "" ]]; then
-    # ((CURSOR--))
-    # BUFFER="$LBUFFER"
-    # if [[ "$LBUFFER" != "" ]] && [[ "$LBUFFER[-1]" != " " ]]; then
-      # zcom::predict
-    # else
-      # zle -M ""
-    # fi
-    # zcom::highlight
-  # fi
-}
-
-function zcom::complete-word()
-{
-  comppostfuncs=( zcom::limit-list )
-  zle complete-word
-  return $?
-}
-
-function zcom::expand-or-complete
-{
-  if [[ "$RBUFFER" == "" ]]; then
-    zle .expand-or-complete
-  else
-    BUFFER="$LBUFFER"
-    zcom::complete-word
-    if [[ "$LBUFFER[-2,-1]" == '\ ' ]] ||
-       [[ "$LBUFFER[-1]" != " " ]]
-    then
-      zcom::complete-word
-    fi
-  fi
 }
 
 zcom::complete-on
