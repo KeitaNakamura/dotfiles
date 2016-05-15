26a48a4e24d1db211c55e61d6c516fb93b6dfe75
Keita Nakamura <nakamura-keita-kn@ynu.jp>
2016-05-16 02:06:16 +0900

    simplify code

--- sh/zcomplete.zsh
+++ sh/zcomplete.zsh
@@ -1,41 +1,39 @@
-autoload -U compinit
+autoload -Uz compinit
 zle -N __zcomplete::complete-on
 zle -N __zcomplete::complete-off
 compinit
 
-ZCOMPLETE_LIST_MAX=40
-
 function __zcomplete::binded-function() {
-  lists="`zle -l`"
-  echo "$lists" | awk '/'$1' / {print $2}' | sed -e "s/(\(.*\))/\1/"
+  local lists="`zle -l`"
+  echo "$lists" | awk '/'^$1' / {print $2}' | sed -e "s/(\(.*\))/\1/"
 }
 
 function __zcomplete::complete-on() {
-  ORIG_INSERT="$(__zcomplete::binded-function "self-insert")"
-  ORIG_COMPLETE="$(__zcomplete::binded-function "expand-or-complete")"
-  zle -N self-insert __zcomplete::self-insert
-  zle -N expand-or-complete __zcomplete::expand-or-complete
+  local widgets=("self-insert" "expand-or-complete" "backward-delete-char")
+  for w in "${widgets[@]}"
+  do
+    zle -N ${w}-orig "$(__zcomplete::binded-function "${w}")"
+    zle -N ${w} __zcomplete::${w}
+  done
 }
 
 function __zcomplete::complete-off() {
-  if [[ "$ORIG_INSERT" == "" ]]; then
-    zle -A .self-insert self-insert
-  else
-    zle -N self-insert $ORIG_INSERT
-  fi
-  if [[ "$ORIG_COMPLETE" == "" ]]; then
-    zle -A .expand-or-complete expand-or-complete
-  else
-    zle -N expand-or-complete $ORIG_COMPLETE
-  fi
+  local widgets=("self-insert" "expand-or-complete" "backward-delete-char")
+  for w in "${widgets[@]}"
+  do
+    if [[ "$(__zcomplete::binded-function "${w}-orig")" == "" ]]; then
+      zle -A .$w $w
+    else
+      zle -A ${w}-orig $w
+      zle -D ${w}-orig
+    fi
+  done
 }
 
 function __zcomplete::limit-list() {
-  if ((compstate[nmatches] <= 1)); then
-    zle -M ""
-  elif ((compstate[list_lines] > $ZCOMPLETE_LIST_MAX)); then
-    compstate[list]=""
-    zle -M "too many matches."
+  if ((compstate[list_lines] + BUFFERLINES + 2 > LINES)); then
+    compstate[list]=''
+    zle -M "Too many matches ($compstate[list_lines] lines)"
   fi
 }
 
@@ -44,37 +42,37 @@ function __zcomplete::highlight() {
 }
 
 function __zcomplete::show-list {
-  if [[ "$LBUFFER[-2,-1]" == '\ ' ]] ||
-     [[ "$LBUFFER[-1]" != ' ' ]]
+  if
+    ((CURSOR > 1)) && (
+    [[ "$LBUFFER[-2,-1]" == '\ ' ]] ||
+    [[ "$LBUFFER[-1]" != ' ' ]]
+    )
   then
     comppostfuncs=( __zcomplete::limit-list )
     zle list-choices
   fi
+  __zcomplete::highlight
 }
 
 function __zcomplete::self-insert() {
   if zle .self-insert; then
-    if
-      ((PENDING == 0)) &&
-      ((CURSOR > 1)) &&
-      [[ "$KEYS[-1]" != " " ]] &&
-      [[ "$PREBUFFER" == "" ]]
-    then
       __zcomplete::show-list
-    else
-      zle -M ""
-    fi
   fi
-  __zcomplete::highlight
 }
 
 function __zcomplete::expand-or-complete {
   if zle complete-word; then
-    __zcomplete::show-list
+      __zcomplete::show-list
   else
     zle .expand-or-complete
+    __zcomplete::highlight
   fi
-  __zcomplete::highlight
+}
+
+function __zcomplete::backward-delete-char {
+  if zle .backward-delete-char; then
+    __zcomplete::show-list
+  fi
 }
 
 __zcomplete::complete-on
