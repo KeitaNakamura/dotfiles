458fb41556873ccff67ac5593fa1e18d78701953
Keita Nakamura <nakamura-keita-kn@ynu.jp>
2016-05-15 13:27:37 +0900

    disable grey suggestion

--- sh/zcomplete.zsh
+++ sh/zcomplete.zsh
@@ -3,7 +3,7 @@ zle -N __zcomplete::complete-on
 zle -N __zcomplete::complete-off
 compinit
 
-ZCOMPLETE_LIST_MAX=20
+ZCOMPLETE_LIST_MAX=40
 ZCOMPLETE_HIGHLIGHT_STYLE='fg=8'
 
 function __zcomplete::binded-function() {
@@ -14,8 +14,12 @@ function __zcomplete::binded-function() {
 function __zcomplete::complete-on() {
   ORIG_INSERT="$(__zcomplete::binded-function "self-insert")"
   ORIG_DELETE="$(__zcomplete::binded-function "backward-delete-char")"
+  # ORIG_FINISH="$(__zcomplete::binded-function "zle-line-finish")"
   zle -N self-insert __zcomplete::self-insert
-  zle -N backward-delete-char __zcomplete::backward-delete-char
+  # zle -N backward-delete-char __zcomplete::backward-delete-char
+  # zle -N complete-word __zcomplete::complete-word
+  # zle -N expand-or-complete __zcomplete::expand-or-complete
+  # zle -N zle-line-finish __zcomplete::line-finish
 }
 
 function __zcomplete::complete-off() {
@@ -29,12 +33,17 @@ function __zcomplete::complete-off() {
   else
     zle -N basckward-delete-char $ORIG_DELETE
   fi
+  # if [[ "$ORIG_FINISH" == "" ]]; then
+    # zle -A .zle-line-finish zle-line-finish
+  # else
+    # zle -N zle-line-finish $ORIG_FINISH
+  # fi
 }
 
 function __zcomplete::limit-list() {
   if ((compstate[nmatches] <= 1)); then
     zle -M ""
-  elif ((compstate[list_lines] > ZCOM_LIST_MAX)); then
+  elif ((compstate[list_lines] > $ZCOMPLETE_LIST_MAX)); then
     compstate[list]=""
     zle -M "too many matches."
   fi
@@ -47,13 +56,34 @@ function __zcomplete::predict() {
   CURSOR="$cursor"
 }
 
+function __zcomplete::show-list {
+  if [[ "$LBUFFER[-2,-1]" == '\ ' ]] ||
+     [[ "$LBUFFER[-1]" != ' ' ]]
+  then
+    comppostfuncs=( __zcomplete::limit-list )
+    zle list-choices
+
+    # rbuffer="$RBUFFER"
+
+    # BUFFER="$LBUFFER"
+    # cursor="$CURSOR"
+    # comppostfuncs=( __zcomplete::limit-list )
+    # zle complete-word
+    # region_highlight+=("$cursor $CURSOR fg=8")
+
+    # rbuffer=`echo "$rbuffer" | sed -e "s/^$BUFFER[$cursor,$CURSOR]//" 2>/dev/null`
+
+    # BUFFER="$BUFFER$rbuffer"
+    # CURSOR="$cursor"
+  fi
+}
+
 function __zcomplete::highlight() {
   _zsh_highlight 2>/dev/null
   region_highlight+=("$CURSOR $#BUFFER fg=8")
 }
 
 function __zcomplete::self-insert() {
-  BUFFER="$LBUFFER"
   if zle .self-insert; then
     if
       ((PENDING == 0)) &&
@@ -61,18 +91,71 @@ function __zcomplete::self-insert() {
       [[ "$KEYS[-1]" != " " ]] &&
       [[ "$PREBUFFER" == "" ]]
     then
-      __zcomplete::predict
+      __zcomplete::show-list
+      # comppostfuncs=( __zcomplete::limit-list )
+      # zle list-choices
     else
       zle -M ""
     fi
   fi
-  __zcomplete::highlight
+
+  # BUFFER="$LBUFFER"
+  # if zle .self-insert; then
+    # if
+      # ((PENDING == 0)) &&
+      # ((CURSOR > 1)) &&
+      # [[ "$KEYS[-1]" != " " ]] &&
+      # [[ "$PREBUFFER" == "" ]]
+    # then
+      # __zcomplete::predict
+    # else
+      # zle -M ""
+    # fi
+  # fi
+  # __zcomplete::highlight
 }
 
 function __zcomplete::backward-delete-char() {
-  ((CURSOR--))
-  BUFFER="$LBUFFER"
-  __zcomplete::highlight
+  # zle .backward-delete-char
+  # __zcomplete::show-list
+  # ((CURSOR--))
+  # BUFFER="$LBUFFER"
+  # __zcomplete::highlight
 }
+
+function __zcomplete::line-finish() {
+  BUFFER="$LBUFFER"
+}
+
+function __zcomplete::complete-word() {
+  comppostfuncs=( __zcomplete::limit-list )
+  zle .complete-word
+  return $?
+}
+
+function __zcomplete::expand-or-complete
+{
+  # BUFFER="$LBUFFER"
+  if zle complete-word; then
+    __zcomplete::show-list
+  else
+    zle .expand-or-complete
+  fi
+  # comppostfuncs=( __zcomplete::limit-list )
+  # zle list-choices
+  # zle -R ""
+  # if [[ "$RBUFFER" == "" ]]; then
+    # zle .expand-or-complete
+  # else
+    # BUFFER="$LBUFFER"
+    # __zcomplete::complete-word
+    # if [[ "$LBUFFER[-2,-1]" == '\ ' ]] ||
+       # [[ "$LBUFFER[-1]" != " " ]]
+    # then
+      # __zcomplete::complete-word
+    # fi
+  # fi
+}
+
 
 __zcomplete::complete-on
