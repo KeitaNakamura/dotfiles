83ea8c83b76e29e584573dfc32294792986761d3
Keita Nakamura <nakamura-keita-kn@ynu.jp>
2016-05-19 00:00:33 +0900

    add texshop colorscheme

---
+++ colorscheme/texshop/solarized_light.sh
@@ -0,0 +1,40 @@
+# solarized light color scheme
+
+# background = solarized base3 = 253 246 227
+defaults write TeXShop background_R 0.988281
+defaults write TeXShop background_G 0.960938
+defaults write TeXShop background_B 0.886719
+
+# commands = solarized red = 220  50  47
+defaults write TeXShop commandred 0.859375
+defaults write TeXShop commandgreen 0.195312
+defaults write TeXShop commandblue 0.183594
+
+# comments = solarized base1 = 147 161 161
+defaults write TeXShop commentred 0.574219
+defaults write TeXShop commentgreen 0.628906
+defaults write TeXShop commentblue 0.628906
+
+# foreground = 75 84 89
+defaults write TeXShop foreground_R 0.292969
+defaults write TeXShop foreground_G 0.328125
+defaults write TeXShop foreground_B 0.347656
+# foreground = solarized base00 = 101 123 131
+# defaults write TeXShop foreground_R 0.394531
+# defaults write TeXShop foreground_G 0.480469
+# defaults write TeXShop foreground_B 0.511719
+
+# index = solarized magenta = 211  54 130
+defaults write TeXShop indexred 0.824219
+defaults write TeXShop indexgreen 0.210938
+defaults write TeXShop indexblue 0.507812
+
+# marker = solarized cyan = 42 161 152
+defaults write TeXShop markerred 0.164062
+defaults write TeXShop markergreen 0.628906
+defaults write TeXShop markerblue 0.59375
+
+# insertionpoint = solarized base00 = 101 123 131
+defaults write TeXShop insertionpoint_R 0.394531
+defaults write TeXShop insertionpoint_G 0.480469
+defaults write TeXShop insertionpoint_B 0.511719
