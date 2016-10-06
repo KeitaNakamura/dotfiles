4d3c6be68ae62a7dba23edc64f12babd812a8359
Keita Nakamura <nakamura-keita-kn@ynu.jp>
2016-10-06 15:26:11 +0900

    add setting for MacVim

--- keyhac/config.py
+++ keyhac/config.py
@@ -1,4 +1,4 @@
-﻿import sys
+import sys
 import os
 import datetime
 import subprocess
@@ -11,11 +11,13 @@ def configure(keymap):
     keymap_global = keymap.defineWindowKeymap()
     keymap_terminal = keymap.defineWindowKeymap( app_name="com.apple.Terminal" )
     keymap_iterm2 = keymap.defineWindowKeymap( app_name="com.googlecode.iterm2" )
+    keymap_macvim = keymap.defineWindowKeymap( app_name="org.vim.MacVim" )
 
     # global
-    keymap_global["O-LCmd"] = "Ctrl-Shift-Semicolon"
-    keymap_global["O-RCmd"] = "Ctrl-Shift-J"
+    # keymap_global["O-LCmd"] = "Ctrl-Shift-Semicolon"
+    # keymap_global["O-RCmd"] = "Ctrl-Shift-J"
 
-    # terminal & iterm2
+    # terminal, iterm2, macvim
     keymap_terminal["Esc"] = "Esc", "Ctrl-Shift-Semicolon"
     keymap_iterm2["Esc"] = "Esc", "Ctrl-Shift-Semicolon"
+    keymap_macvim["Esc"] = "Esc", "Ctrl-Shift-Semicolon"
